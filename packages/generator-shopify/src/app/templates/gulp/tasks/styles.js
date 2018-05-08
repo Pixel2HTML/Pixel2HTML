0cf2792c9a58b196fee73a8f14d2b66fe8469116
Miguel Palau Zarza <mpalauzarza@gmail.com>
2018-05-08 11:54:54 -0500

    actually tweaking the styles on the correct place

--- packages/generator-shopify/src/app/templates/gulp/tasks/styles.js
+++ packages/generator-shopify/src/app/templates/gulp/tasks/styles.js
@@ -7,32 +7,51 @@ const moduleImporter = require('sass-module-importer')
 
 const destination = config.theme + '/assets'
 
-gulp.task('main:styles', function () {
-  return gulp.src(config.src.styles + '/main.scss')
+// Remove unused styles config
+const purifyWatch = [
+  '.deploy/layout/**/*.liquid',
+  '.deploy/snippets/**/*.liquid',
+  '.deploy/sections/**/*.liquid',
+  '.deploy/templates/**/*.liquid',
+  '.deploy/assets/**/*.js',
+  '.deploy/assets/**/*.js.liquid',
+]
+
+gulp.task('main:styles', () =>
+  gulp.src(config.src.styles + '/main.scss')
     .pipe(when(!production, $.sourcemaps.init()))
-    .pipe($.sass({importer: moduleImporter()}))
+    .pipe($.sass({ importer: moduleImporter() }))
     .on('error', $.sass.logError)
-    .pipe($.autoprefixer({browsers: config.browsers}))
+    .pipe($.autoprefixer({
+      browsers: config.browsers
+    }))
     .pipe(when(production, $.groupCssMediaQueries()))
     .pipe(when(production, $.csscomb()))
-    .pipe(when(!production, $.sourcemaps.write('.', {sourceMappingURL: config.makeLiquidSourceMappingURL})))
+    .pipe(when(production, $.purifycss(purifyWatch, { info: true })))
+    .pipe(when(!production, $.sourcemaps.write('.', {
+      sourceMappingURL: config.makeLiquidSourceMappingURL
+    })))
     .pipe(when(production, $.cssnano()))
     .pipe($.rename(config.appendLiquidExt)) // main.css.liquid
     .pipe(gulp.dest(destination))
-})
+)
 
-gulp.task('vendor:styles', function () {
-  return gulp.src(config.src.styles + '/vendor.scss')
+gulp.task('vendor:styles', () =>
+  gulp.src(config.src.styles + '/vendor.scss')
     .pipe(when(!production, $.sourcemaps.init()))
-    .pipe($.sass({importer: moduleImporter()}))
+    .pipe($.sass({ importer: moduleImporter() }))
     .on('error', $.sass.logError)
-    .pipe($.autoprefixer({browsers: config.browsers}))
+    .pipe($.autoprefixer({
+      browsers: config.browsers
+    }))
     .pipe(when(production, $.groupCssMediaQueries()))
     .pipe(when(production, $.csscomb()))
-    .pipe(when(!production, $.sourcemaps.write('.', {sourceMappingURL: config.makeLiquidSourceMappingURL})))
+    .pipe(when(!production, $.sourcemaps.write('.', {
+      sourceMappingURL: config.makeLiquidSourceMappingURL
+    })))
     .pipe(when(production, $.cssnano()))
     .pipe($.rename(config.appendLiquidExt)) // vendor.css.liquid
     .pipe(gulp.dest(destination))
-})
+)
 
 gulp.task('styles', gulp.parallel('main:styles', 'vendor:styles'))
