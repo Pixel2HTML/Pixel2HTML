92f8e9b39a78cebbb0d4c12b0c4d519b66189c51
Miguel Palau Zarza <mpalauzarza@gmail.com>
2018-05-10 20:56:21 -0500

    updating our example code

--- packages/generator-shopify/example/src/scripts/async-button.js
+++ packages/generator-shopify/example/src/scripts/async-button.js
@@ -1,11 +1,14 @@
 const asyncButton = button => {
+  // Change background color
   const html = document.documentElement
   html.style.backgroundColor = '#31a8fa'
+
+  // Brag about it
   const h4 = document.createElement('h4')
   h4.textContent = 'This was added async!'
   button.parentNode.appendChild(h4)
-
   console.log(`You have been removed from another chunk async!`)
+
   // Remove button after we're done
   button.parentNode.removeChild(button)
 }
