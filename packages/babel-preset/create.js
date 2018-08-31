2be6bdc0f74bb3c32919b958f0c22c07d2ad5b13
Miguel Palau Zarza <mpalauzarza@gmail.com>
2018-08-31 12:42:50 -0500

    removing babel runtime polyfill

--- packages/babel-preset/create.js
+++ packages/babel-preset/create.js
@@ -101,7 +101,6 @@ module.exports = function (api, opts, env) {
         require('@babel/plugin-transform-runtime').default,
         {
           helpers: false,
-          polyfill: false,
           regenerator: true,
         },
       ],
